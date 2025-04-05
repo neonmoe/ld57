af58c08c7f0f145d2433caa654d50e47bdcd2860
Jens Pitkanen <jens@neon.moe>
2025-04-05 16:33:12 +0300

    Add character, job station, resource game objects

---
+++ game-lib/src/game_object.rs
@@ -0,0 +1,139 @@
+use core::ops::Deref;
+
+use bytemuck::{Pod, Zeroable};
+use engine::impl_game_object;
+use glam::IVec2;
+
+// Game objects
+
+#[derive(Debug, Zeroable)]
+pub struct Character {
+    pub status: CharacterStatus,
+    pub position: TilePosition,
+    pub held: Stockpile,
+}
+impl_game_object! {
+    impl GameObject for Character using components {
+        status: CharacterStatus,
+        position: TilePosition,
+        held: Stockpile,
+    }
+}
+
+#[derive(Debug, Zeroable)]
+pub struct Resource {
+    pub position: TilePosition,
+    pub stockpile: Stockpile,
+}
+impl_game_object! {
+    impl GameObject for Resource using components {
+        position: TilePosition,
+        stockpile: Stockpile,
+    }
+}
+
+#[derive(Debug, Zeroable)]
+pub struct JobStation {
+    pub position: TilePosition,
+    pub stockpile: Stockpile,
+    pub status: JobStationStatus,
+}
+impl_game_object! {
+    impl GameObject for JobStation using components {
+        position: TilePosition,
+        stockpile: Stockpile,
+        status: JobStationStatus,
+    }
+}
+
+// Components
+
+#[derive(Clone, Copy, Debug, Zeroable, Pod)]
+#[repr(C)]
+pub struct CharacterStatus {
+    pub brain_index: usize,
+}
+
+#[derive(Clone, Copy, Debug, Zeroable, Pod)]
+#[repr(C)]
+pub struct JobStationStatus {
+    pub variant: JobStationVariant,
+    pub work_invested: u8,
+}
+impl JobStationStatus {
+    pub const fn details(self) -> Option<JobStationDetails> {
+        match self.variant {
+            JobStationVariant::ENERGY_GENERATOR => Some(JobStationDetails {
+                resource_variant: ResourceVariant::MAGMA,
+                resource_amount: 1,
+                work_ms_amount: 1_000,
+                output_variant: ResourceVariant::ENERGY,
+                output_amount: 1,
+            }),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Zeroable, Pod)]
+#[repr(C)]
+pub struct Stockpile {
+    pub variant_count: u8,
+    pub variants: [ResourceVariant; 4],
+    pub amounts: [u8; 4],
+}
+impl Stockpile {
+    pub const fn with_resource(mut self, resource: ResourceVariant, amount: u8) -> Stockpile {
+        let i = self.variant_count as usize;
+        if i >= self.variants.len() {
+            self
+        } else {
+            self.variants[i] = resource;
+            self.amounts[i] = amount;
+            self.variant_count += 1;
+            self
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Zeroable, Pod)]
+#[repr(C)]
+pub struct TilePosition(pub IVec2);
+impl TilePosition {
+    pub fn new(x: i32, y: i32) -> TilePosition {
+        TilePosition(IVec2 { x, y })
+    }
+}
+impl Deref for TilePosition {
+    type Target = IVec2;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+// Other
+
+pub struct JobStationDetails {
+    pub resource_variant: ResourceVariant,
+    pub resource_amount: u8,
+    pub work_ms_amount: u16,
+    pub output_variant: ResourceVariant,
+    pub output_amount: u8,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Zeroable, Pod)]
+#[repr(C)]
+pub struct JobStationVariant(u8);
+impl JobStationVariant {
+    pub const NONE: JobStationVariant = JobStationVariant(0);
+    pub const ENERGY_GENERATOR: JobStationVariant = JobStationVariant(1);
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Zeroable, Pod)]
+#[repr(C)]
+pub struct ResourceVariant(u8);
+impl ResourceVariant {
+    pub const NONE: ResourceVariant = ResourceVariant(0);
+    pub const MAGMA: ResourceVariant = ResourceVariant(1);
+    pub const ENERGY: ResourceVariant = ResourceVariant(2);
+}
--- game-lib/src/lib.rs
+++ game-lib/src/lib.rs
@@ -1,10 +1,19 @@
 #![no_std]
 
 mod camera;
+mod game_object;
 mod tilemap;
 
+use bytemuck::Zeroable;
 use camera::Camera;
-use engine::{Engine, allocators::LinearAllocator, renderer::DrawQueue};
+use engine::{
+    Engine, allocators::LinearAllocator, define_system, game_objects::Scene, geom::Rect,
+    renderer::DrawQueue, resources::sprite::SpriteHandle,
+};
+use game_object::{
+    Character, CharacterStatus, JobStation, JobStationStatus, JobStationVariant, Resource,
+    ResourceVariant, Stockpile, TilePosition,
+};
 use glam::Vec2;
 use platform::{Instant, Platform};
 use tilemap::Tilemap;
@@ -12,15 +21,52 @@ use tilemap::Tilemap;
 #[repr(u8)]
 enum DrawLayer {
     Tilemap,
+    GameObjects,
 }
 
 pub struct Game {
     tilemap: Tilemap<'static>,
     camera: Camera,
+    scene: Scene<'static>,
+    placeholder_sprite: SpriteHandle,
 }
 
 impl Game {
     pub fn new(arena: &'static LinearAllocator, engine: &Engine) -> Game {
+        let mut scene = Scene::builder()
+            .with_game_object_type::<Character>(10)
+            .with_game_object_type::<JobStation>(100)
+            .with_game_object_type::<Resource>(1000)
+            .build(arena, &engine.frame_arena)
+            .unwrap();
+
+        let char_spawned = scene.spawn(Character {
+            status: CharacterStatus { brain_index: 0 }, // TODO: implement brains
+            position: TilePosition::new(5, 1),
+            held: Stockpile::zeroed(),
+        });
+        debug_assert!(char_spawned.is_ok());
+
+        let job_station_spawned = scene.spawn(JobStation {
+            position: TilePosition::new(7, 7),
+            stockpile: Stockpile::zeroed(),
+            status: JobStationStatus {
+                variant: JobStationVariant::ENERGY_GENERATOR,
+                work_invested: 0,
+            },
+        });
+        debug_assert!(job_station_spawned.is_ok());
+
+        for y in 4..7 {
+            for x in 1..4 {
+                let res_spawned = scene.spawn(Resource {
+                    position: TilePosition::new(x, y),
+                    stockpile: Stockpile::zeroed().with_resource(ResourceVariant::MAGMA, 2),
+                });
+                debug_assert!(res_spawned.is_ok());
+            }
+        }
+
         Game {
             tilemap: Tilemap::new(arena, &engine.resource_db),
             camera: Camera {
@@ -28,6 +74,8 @@ impl Game {
                 size: Vec2::ZERO,
                 output_size: Vec2::ZERO,
             },
+            scene,
+            placeholder_sprite: engine.resource_db.find_sprite("Placeholder").unwrap(),
         }
     }
 
@@ -48,6 +96,28 @@ impl Game {
             &self.camera,
             &engine.frame_arena,
         );
+
+        let placeholder_sprite = engine.resource_db.get_sprite(self.placeholder_sprite);
+        let scale = self.camera.output_size / self.camera.size;
+        self.scene
+            .run_system(define_system!(|_, tile_positions: &[TilePosition]| {
+                for tile_pos in tile_positions {
+                    let dst = Rect::xywh(
+                        tile_pos.x as f32 * scale.x,
+                        tile_pos.y as f32 * scale.y,
+                        scale.x,
+                        scale.y,
+                    );
+                    let draw_success = placeholder_sprite.draw(
+                        dst,
+                        DrawLayer::GameObjects as u8,
+                        &mut draw_queue,
+                        &engine.resource_db,
+                        &mut engine.resource_loader,
+                    );
+                    debug_assert!(draw_success);
+                }
+            }));
 
         draw_queue.dispatch_draw(&engine.frame_arena, platform);
     }
